ebf506de038560f1aa1d16108b7ef0ab86f0336e
clsmartinez <cls.martinez@outlook.com>
2025-12-16 16:18:27 +0100

    Eliminar archivos

--- DW en Contorno Cliente/Tema2/ExerciciosPracticos2.1/exercicio03.js
+++
@@ -1,9 +0,0 @@
-let num1= prompt("Introduce un número");
-let num2= prompt("Introduce o segundo número");
-
-parseInt(num1);parseInt(num2);
-
-let suma= num1+num2;
-let media1=num1/suma;
-let media2=num2/suma;
-
--- DW en Contorno Cliente/Tema3/ExemplosTema3/exemplo07.js
+++
@@ -1,7 +0,0 @@
-let cousa=document.getElementById("cousa");
-
-console.log(cousa.getAttribute("texto"));
-
-cousa.setAttribute("texto","Novo valor");
-
-console.log(cousa.getAttribute("texto"));
