10644dc724ff196e254c462f5d12a64f8633e5b3
clsmartinez <cls.martinez@outlook.com>
2025-12-16 16:25:28 +0100

    Eliminar pdf del repositorio

--- DW en Contorno Cliente/Tema2/ExerciciosPracticos2.1/exercicio01.js
+++
@@ -1,9 +0,0 @@
-//Mensaxes de consola
-console.log("Esta é unha mensaxe de proba");
-console.warn("Esta é unha mensaxe de alerta");
-console.error("Esta é unha mensaxe de error");
-//Amosar un diálogo emerxente cun texto
-prompt("Este texto é un diálogo emerxente.");
-//Solicitar ao usuario o seu nome, recollelo mediante un diálogo de consulta e amosalo por consola.
-let resultado=prompt("Escribe aquí algo")
-console.log("Has escrito: " + resultado);
--- DW en Contorno Cliente/Tema3/practicaExame/pregunta9.js
+++
@@ -1,3 +0,0 @@
-let orixinal=[];
-
-let copia=[...orixinal];
